35d1950bdac6cd3e1a47c58795e92f57d82d68db
C. Michael Fisher <Haeshka@gmail.com>
2022-02-13 17:04:20 -0500

    Update just_make_it.py

--- just_make_it.py
+++ just_make_it.py
@@ -1,6 +1,6 @@
 # Just do it
 import pyinputplus as pyip
-leader_ability_bonus = ["Banner Bearer", "Craftsman", "Herald", 
+leader_ability_bonus = ["Banner Bearer", "Craftsman", "Herald",
                "Scholar", "Pioneer", "Scavenger"]
 
 spells = ["Might", "Fervor", "Stalwart" , "Ward", "Celerity", "Forced March",
@@ -8,16 +8,41 @@ spells = ["Might", "Fervor", "Stalwart" , "Ward", "Celerity", "Forced March",
          "Silver-Tongue", "True-Cause", "Napoleon", "Shroud", "Telescoping",
          "Runesmith", "Mind Leech", "Force Cage", "Earth Golem"]
 
-def which_leader_spells():
-    print("What spells does your leader know?")
-    while choice not '':
+def which_chief_spells(chief_spells_known):
+    print("What spells does your Chief know?")
+    choice = 1
+    while choice != '':
         choice = pyip.inputMenu(spells, numbered=True, blank=True)
-    leader_spells_known
+        if choice == '':
+            break
+        else:
+            chief_spells_known.append(choice)
+    return chief_spells_known
 
+def which_cohort_spells(cohort_spells_known):
+    print("What spells does your Cohort know?")
+    choice = 1
+    while choice != '':
+        choice = pyip.inputMenu(spells, numbered=True, blank=True)
+        if choice == '':
+            break
+        else:
+            cohort_spells_known.append(choice)
+    return cohort_spells_known
 
 def display_leader_info():
     print("Your Chief's name is: {}".format(chief_name))
+    print("Your Chief's Leader Ability is: {}".format(chief_leader_ability_bonus))
+    print("Your Chief knows the following spells: ")
+    for x in chief_spells_known:
+        print(x)
     print("Your Cohort's name is: {}".format(cohort_name))
+    print("Your Cohort's Leader Ability is: {}".format(cohort_leader_ability_bonus))
+    print("Your Cohort knows the following spells: ")
+    for x in cohort_spells_known:
+        print(x)
+    
+    
 
 
 
@@ -40,16 +65,20 @@ def main():
         leader_ability_bonus.remove(chief_leader_ability_bonus)
         print("\n")
         print("Which leader bonus do you select for your Cohort?")
-        chief_leader_ability_bonus=pyip.inputMenu(leader_ability_bonus, blank=False)
+        cohort_leader_ability_bonus=pyip.inputMenu(leader_ability_bonus, blank=False)
         print("\n")
         #######################################################################
-        leader_spells_known= []
-        
-        
+        chief_spells_known= []
+        which_chief_spells(chief_spells_known)
+        cohort_spells_known= []
+        which_cohort_spells(cohort_spells_known)
+        #######################################################################
+        display_leader_info()
+
         #######################################################################
         # Create Heroes
         #######################################################################
-        
+
         #######################################################################
         # Create Squads
         #######################################################################
