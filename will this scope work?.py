a099d693b72345d86e4d0468714788cfb35fcc58
C. Michael <Haeshka@gmail.com>
2022-05-08 08:34:27 -0400

    Delete will this scope work?.py

--- will this scope work?.py
+++
@@ -1,8 +0,0 @@
-import pyinputplus as pyip
-num_of_heroes = pyip.inputInt(prompt="How many heroes? ", blank=True)
-global hero_info
-hero_info = []
-for x in range(1, num_of_heroes+1):
-    hero_info.append("hero{}".format(int(x)))
-    
-print(hero_info)
